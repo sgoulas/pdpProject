6b00a1cff9199c44435689c1e925e5bd293b3734
sgoulas <sprgoulas@gmail.com>
2021-07-04 23:25:33 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
-const App: React.FC = () => (
-    <h1 style={{ color: 'orange' }}>Hello World! sgoulas</h1>
-);
+const App: React.FC = () => {
+    return <h1 style={{ color: 'orange' }}>Hello World! sgoulas</h1>;
+};
 
 export default App;
