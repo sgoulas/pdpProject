6336fd48afd1acbd0ccd89ce49711d1836706063
Spyros Goulas <Spyros.Goulas@gfk.com>
2021-08-04 13:05:53 +0300

    test

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
 
-const App: React.FC = () => <h1 style={{ color: 'orange' }}>Hello World!!</h1>;
+const App: React.FC = () => <h1 style={{ color: 'orange' }}>Hello World!</h1>;
 
 export default App;
