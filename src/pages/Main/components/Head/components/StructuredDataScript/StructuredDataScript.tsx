d9dd83524cc7b2c8ad779f8fbe9006fdd8bf4487
sgoulas <sprgoulas@gmail.com>
2021-09-02 02:28:20 +0300

    adds some json ld data

--- src/pages/Main/components/Head/components/StructuredDataScript/StructuredDataScript.tsx
+++ src/pages/Main/components/Head/components/StructuredDataScript/StructuredDataScript.tsx
@@ -1,17 +1,24 @@
 import React from 'react';
 
-import { GIT_REPO_URL, SITE_NAME, SITE_URL } from '@core';
+import { GIT_REPO_URL, SITE_IMAGE_URL, SITE_NAME, SITE_URL } from '@core';
 
 const StructuredDataScript: React.FC = () => {
     const JSON_LD = {
         '@context': 'http://schema.org',
         '@type': 'Organization',
         name: SITE_NAME,
+        description: 'An ecommerce site selling technology products',
         url: SITE_URL,
+        image: [SITE_IMAGE_URL],
         sameAs: [GIT_REPO_URL],
-        email: 'sprgoulas@gmail.com',
+        contactPoint: {
+            contactType: 'PR',
+            email: 'sprgoulas@gmail.com',
+            hoursAvailable: ['Mo-Fri 09:00-21:00', 'Sa-Su 11:00-13:00'],
+        },
         founder: 'Spyros Goulas',
         foundingDate: '2021-5-15',
+        numberOfEmployees: '1',
     };
 
     return (
