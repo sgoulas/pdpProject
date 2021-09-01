730f0f64652ce26d30e158f40f5cc5e13c67f94f
sgoulas <sprgoulas@gmail.com>
2021-09-02 02:35:14 +0300

    diary entry, wraps footer in article tag

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -16,38 +16,48 @@ const Footer: React.FC = () => {
     const classes = useStyles();
 
     return (
-        <footer className={classes.footer}>
-            <div className={classes.container}>
-                <div className={classes.containerItem}>
-                    <Typography component="h4" variant="h6">
-                        Social
-                    </Typography>
-                    <LinkExternal url={LINKEDIN_PROFILE_URL} text="linkedIn" />
-                    <LinkExternal
-                        url={STACK_OVERFLOW_PROFILE_URL}
-                        text="stack overflow"
-                    />
-                    <LinkExternal url={DEV_PROFILE_URL} text="DEV.to" />
+        <article>
+            <footer className={classes.footer}>
+                <div className={classes.container}>
+                    <div className={classes.containerItem}>
+                        <Typography component="h4" variant="h6">
+                            Social
+                        </Typography>
+                        <LinkExternal
+                            url={LINKEDIN_PROFILE_URL}
+                            text="linkedIn"
+                        />
+                        <LinkExternal
+                            url={STACK_OVERFLOW_PROFILE_URL}
+                            text="stack overflow"
+                        />
+                        <LinkExternal url={DEV_PROFILE_URL} text="DEV.to" />
+                    </div>
+                    <div className={classes.containerItem}>
+                        <Typography component="h4" variant="h6">
+                            FAQ
+                        </Typography>
+                        <Typography variant="body1">
+                            about this project
+                        </Typography>
+                    </div>
+                    <div className={classes.containerItem}>
+                        <Typography component="h4" variant="h6">
+                            github
+                        </Typography>
+                        <LinkExternal
+                            url={GIT_REPO_URL}
+                            text="project repository"
+                        />
+                        <LinkExternal
+                            url={GIT_PROFILE_URL}
+                            text="github profile"
+                        />
+                    </div>
                 </div>
-                <div className={classes.containerItem}>
-                    <Typography component="h4" variant="h6">
-                        FAQ
-                    </Typography>
-                    <Typography variant="body1">about this project</Typography>
-                </div>
-                <div className={classes.containerItem}>
-                    <Typography component="h4" variant="h6">
-                        github
-                    </Typography>
-                    <LinkExternal
-                        url={GIT_REPO_URL}
-                        text="project repository"
-                    />
-                    <LinkExternal url={GIT_PROFILE_URL} text="github profile" />
-                </div>
-            </div>
-            <Copyright />
-        </footer>
+                <Copyright />
+            </footer>
+        </article>
     );
 };
 
