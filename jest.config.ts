2606d89357b2c3a4f7e07caee76ebc77a27bf4c3
Spyros Goulas <Spyros.Goulas@gfk.com>
2021-09-27 15:23:40 +0300

    ignores some paths from testing, fixes next vulnerability issue by upgrading

--- jest.config.ts
+++ jest.config.ts
@@ -24,6 +24,10 @@ export default {
         'src/**/*.{ts,tsx}',
         '!src/core/**',
         '!src/**/index.{ts,tsx}',
+        '!src/api/**',
+        '!src/pages/_app.tsx',
+        '!src/pages/_document.tsx',
+        '!src/**/gql.ts',
     ],
 
     // The directory where Jest should output its coverage files
@@ -135,9 +139,7 @@ export default {
     // rootDir: undefined,
 
     // A list of paths to directories that Jest should use to search for files in
-    // roots: [
-    //   "<rootDir>"
-    // ],
+    roots: ['<rootDir>/src'],
 
     // Allows you to use a custom runner instead of Jest's default test runner
     // runner: "jest-runner",
