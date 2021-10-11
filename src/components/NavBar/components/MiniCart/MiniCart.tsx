b80ca270151a12bac6957b9dff5badbf17a56a45
sgoulas <sprgoulas@gmail.com>
2021-10-11 03:24:07 +0300

    styles components, adds checkout button

--- src/components/NavBar/components/MiniCart/MiniCart.tsx
+++ src/components/NavBar/components/MiniCart/MiniCart.tsx
@@ -2,21 +2,35 @@ import React, { useState } from 'react';
 import IconButton from '@material-ui/core/IconButton';
 import ShoppingCart from '@material-ui/icons/ShoppingCartOutlined';
 import SwipeableDrawer from '@material-ui/core/SwipeableDrawer';
+import Grid from '@material-ui/core/Grid';
+import Box from '@material-ui/core/Box';
+import Button from '@material-ui/core/Button';
+import { useRouter } from 'next/router';
 
 import { useCart } from '@hooks';
+import { Divider, Typography } from '@components';
 
 import { MiniCartProductCard } from './components';
+import useStyles from './MiniCart.styles';
+import { checkoutPage } from '@core';
 
 //todo maybe change products to items?
 //todo add a css animation for "you have no items in your cart" message
 //todo add counter to the cart icon to indicate items quantity
 
 const MiniCart: React.FC = () => {
-    const { products: items } = useCart();
+    const classes = useStyles();
+    const { products: items, totalPrice } = useCart();
+    const router = useRouter();
     const [isOpen, setIsOpen] = useState(false);
 
     const openCart = () => setIsOpen(true);
     const closeCart = () => setIsOpen(false);
+
+    const goToCheckoutPage = () => {
+        closeCart();
+        router.push(checkoutPage());
+    };
 
     return (
         <>
@@ -30,9 +44,47 @@ const MiniCart: React.FC = () => {
                 onClose={closeCart}
             >
                 this is the mini cart
+                <Divider variant="middle" />
                 {items.map(item => (
                     <MiniCartProductCard key={item.product.id} item={item} />
                 ))}
+                <Divider variant="middle" />
+                <Grid
+                    container
+                    direction="row"
+                    justifyContent="space-between"
+                    alignItems="center"
+                >
+                    <Grid item>
+                        <Box ml={2}>
+                            <Typography variant="body1">
+                                total price:
+                            </Typography>
+                        </Box>
+                    </Grid>
+                    <Grid item>
+                        <Box mr={2}>
+                            <Typography variant="h5" component="p">
+                                {totalPrice}€
+                            </Typography>
+                        </Box>
+                    </Grid>
+                </Grid>
+                <Box textAlign="center" mt={8} mr="auto" ml="auto">
+                    <Button
+                        data-testid="checkout-button"
+                        variant="text"
+                        style={{
+                            backgroundColor: '#FFA41C',
+                        }}
+                        className={classes.checkoutBtn}
+                        onClick={goToCheckoutPage}
+                    >
+                        <Typography variant="body1" component="span">
+                            checkout
+                        </Typography>
+                    </Button>
+                </Box>
             </SwipeableDrawer>
         </>
     );
--- src/components/NavBar/components/MiniCart/components/MiniCartProductCard/MiniCartProductCard.tsx
+++ src/components/NavBar/components/MiniCart/components/MiniCartProductCard/MiniCartProductCard.tsx
@@ -1,6 +1,10 @@
 import React from 'react';
+import Box from '@material-ui/core/Box';
+import Grid from '@material-ui/core/Grid';
+// import Link from 'next/link';
 
 import { CartProduct } from '@core';
+import { ImageWithFallback, Typography } from '@components';
 
 export interface MiniCartProductCardProps {
     item: CartProduct;
@@ -9,7 +13,33 @@ export interface MiniCartProductCardProps {
 //https://codepen.io/btcrooks/pen/MbNape
 
 const MiniCartProductCard: React.FC<MiniCartProductCardProps> = ({
-    item,
-}: MiniCartProductCardProps) => <span>{item.product.name}</span>;
+    item: { product, quantity },
+}: MiniCartProductCardProps) => (
+    <article>
+        <Grid
+            container
+            direction="row"
+            justifyContent="flex-start"
+            alignItems="flex-start"
+        >
+            <Grid item xs={4}>
+                <Box component="div" display="block">
+                    <ImageWithFallback
+                        src={product.image ?? ''}
+                        fallbackSrc={'phoneFallBack.png'}
+                        alt={product.name}
+                        width={100}
+                        height={100}
+                    />
+                </Box>
+            </Grid>
+            <Grid item xs={8}>
+                <Typography>
+                    {product.name} x {quantity}
+                </Typography>
+            </Grid>
+        </Grid>
+    </article>
+);
 
 export default MiniCartProductCard;
