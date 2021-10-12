2a850abd43df59fe2419074ea2fb47bcf1ee740e
sgoulas <sprgoulas@gmail.com>
2021-10-12 17:49:22 +0300

    update site name tests

--- src/components/NavBar/components/SiteName/SiteName.test.tsx
+++ src/components/NavBar/components/SiteName/SiteName.test.tsx
@@ -1,12 +1,28 @@
 import React from 'react';
 
-import { render } from '@testUtils';
+import { renderWithProviders } from '@testUtils';
 
 import SiteName from './SiteName';
+import { landingPage, SITE_NAME } from '@core';
 
 describe('SiteName', () => {
     it('matches snapshot', () => {
-        const { container } = render(<SiteName />);
+        const { container } = renderWithProviders(<SiteName />);
         expect(container.firstChild).toMatchSnapshot();
+    });
+
+    it('navigates to landing page', () => {
+        // eslint-disable-next-line @typescript-eslint/no-var-requires
+        const useRouter = jest.spyOn(require('next/router'), 'useRouter');
+        const mockPush = jest.fn();
+        useRouter.mockImplementation(() => ({ push: mockPush }));
+
+        const { getByText } = renderWithProviders(<SiteName />);
+
+        const siteName = getByText(SITE_NAME);
+
+        siteName.click();
+
+        expect(mockPush).toHaveBeenLastCalledWith(landingPage());
     });
 });
